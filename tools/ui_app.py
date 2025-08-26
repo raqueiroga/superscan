907cd2ed9667b6b6a1275dd67f29dd604a585fd1
raqueiroga <raqueiroga@gmail.com>
2025-08-26 13:00:17 -0300

    Update ui_app.py

--- tools/ui_app.py
+++ tools/ui_app.py
@@ -1,1 +1,5 @@
+import streamlit as st
 
+st.set_page_config(page_title="SuperScan", layout="wide")
+st.title("🚀 SuperScan")
+st.write("Se você está vendo isso, a interface do SuperScan está funcionando!")
